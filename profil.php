bbfe5a7a8db2ab6875103b33284d1f274b597943
Loïcia Saint-Félix <75837991+LoiciaSF@users.noreply.github.com>
2021-03-25 23:25:25 +0100

    finitions3

--- profil.php
+++ profil.php
@@ -16,33 +16,33 @@
 		</div>
 		<div class="mb-3">
 			<label for="address">Adresse postale</label>
-			<input type="text" class="form-control input-flex" id="address" name="Adresse postale" value="56 Rue de la Bonnebière">
-			<button type="button" class="form-button input-flex" id="pseudo" name="pseudo">Modifier</button>
+			<input type="text" class="form-control" id="address" name="Adresse postale" value="56 Rue de la Bonnebière">
+			<button type="button" class="form-button" id="adress" name="adress">Modifier</button>
 		</div>
 		<div class="mb-3">
 			<label for="phone">Téléphone</label>
-			<input type="tel" class="form-control input-flex" id="phone" name="telephone" pattern="[0-9]{10}" value="01 56 44 78 99">
-			<button type="button" class="form-button input-flex" id="pseudo" name="pseudo">Modifier</button>
+			<input type="tel" class="form-control" id="phone" name="telephone" pattern="[0-9]{10}" value="01 56 44 78 99">
+			<button type="button" class="form-button" id="tel" name="tel">Modifier</button>
 		</div>
 		<div class="mb-3">
 			<label for="pseudo" class="form-label">Pseudo</label>
-			<input type="text" class="form-control input-flex" id="pseudo" name="pseudo" value="Pierre54">
-			<button type="button" class="form-button input-flex" id="pseudo" name="pseudo">Modifier</button>
+			<input type="text" class="form-control" id="pseudo" name="pseudo" value="Pierre54">
+			<button type="button" class="form-button" id="pseudo" name="pseudo">Modifier</button>
 		</div>
 		<div class="mb-3">
 			<label for="email" class="form-label">E-mail</label>
-			<input type="email" class="form-control input-flex" id="email" name="email" value="pierre-tylurrude@gmail.com">
-			<button type="button" class="form-button input-flex" id="pseudo" name="pseudo">Modifier</button>
+			<input type="email" class="form-control" id="email" name="email" value="pierre-tylurrude@gmail.com">
+			<button type="button" class="form-button" id="email" name="email">Modifier</button>
 		</div>
 		<div class="mb-3">
 			<label for="password" class="form-label">Mot de passe</label>
-			<input type="password" class="form-control input-flex input-style" id="password" name="password" value="dhoefnzf4">
-			<button type="button" class="form-button input-flex" id="pseudo" name="pseudo">Modifier</button>
+			<input type="password" class="form-control" id="password" name="password" value="dhoefnzf4">
+			<button type="button" class="form-button" id="password" name="password">Modifier</button>
 		</div>
 		<div class="mb-3">
 			<label for="password" class="form-label">Nouveau mot de passe</label>
-			<input type="password" class="form-control input-flex" id="new-password" name="new-password" value="dhoefnzf56">
-			<button type="button" class="form-button input-flex" id="pseudo" name="pseudo">Modifier</button>
+			<input type="password" class="form-control" id="new-password" name="new-password" value="dhoefnzf56">
+			<button type="button" class="form-button" id="new-password" name="new-password">Modifier</button>
 		</div>
 		<div class=button-registration>
 			<input type="submit" value="Enregistrer les informations" class="login-registration" name="envoyer"
